a1dfeb745d297bb37078f6fd5d7820ec94bb47cc
Ritankar Bhattacharjee <rick.bhardwaj27@gmail.com>
2025-05-17 00:41:00 +0530

    initial convex setup successful

---
+++ convex/schema.ts
@@ -0,0 +1,13 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export const User = {
+  email: v.string(),
+  clerkId: v.string(),
+  imageUrl: v.optional(v.string()),
+  fullName: v.optional(v.string()),
+};
+
+export default defineSchema({
+  users: defineTable(User),
+});
